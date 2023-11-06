f18ad2ca11fa97e2182783d443ce6011d91c8f8e
philbadcode <phillip.hail@yahoo.com>
2023-11-06 12:25:45 -0600

    test listview

--- app/skills-page.js
+++ app/skills-page.js
@@ -9,3 +9,16 @@ export function navBack(args) {
   const page = args.object.page;
   page.frame.goBack();
 }
+
+const ObservableArray = require("@nativescript/core/data/observable-array").ObservableArray;
+const listViewModule = require("@nativescript/core/ui/list-view");
+exports.pageLoaded = function(args) {
+  const page = args.object;
+  const listView = page.getViewById("myListView");
+  const itemList = new ObservableArray([
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+  ]);
+  listView.items = itemList;
+  listView.itemTemplate = `<Label text="{{ name }}" />`;
+};
